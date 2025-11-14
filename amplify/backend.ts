d867867490d715564c559872ba4b7ed64e7afac5
dgandhi62 <gandhidhyan@gmail.com>
2025-11-14 13:50:24 -0500

    set deletionprotection

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,12 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+for (const table of Object.values(backend.data.resources.cfnResources.amplifyDynamoDbTables))
+{   
+    (table as any).cfnTable.addPropertyOverride('DeletionProtectionEnabled', false);
+}
